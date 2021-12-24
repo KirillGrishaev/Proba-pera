cb42351cb43388d67d0bb8a5cf4753dba57f95c6
KirillGrishaev <94450408+KirillGrishaev@users.noreply.github.com>
2021-12-25 10:51:31 +1100

    Delete OtdelPay.java

--- src/bank/otdel/OtdelPay.java
+++
@@ -1,8 +0,0 @@
-package bank.otdel;
-
-public class OtdelPay extends OtdelConstruct{
-    public OtdelPay (String name,int rabotnik){
-        this.otdelName = otdelName;
-        this.rabotnik = rabotnik;
-}
-}
