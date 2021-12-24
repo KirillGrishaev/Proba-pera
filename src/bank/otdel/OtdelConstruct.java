68073734953baa058367e90758bb110ec0534a31
KirillGrishaev <94450408+KirillGrishaev@users.noreply.github.com>
2021-12-25 10:51:04 +1100

    Delete OtdelConstruct.java

--- src/bank/otdel/OtdelConstruct.java
+++
@@ -1,6 +0,0 @@
-package bank.otdel;
-
-public class OtdelConstruct {
-    protected String otdelName;//название отдела//
-    int rabotnik ;//число сотрудников отдела, сколько клиентов за раз можно обработать//
-}
