d81b1bb77cd7898aa70a386e714bb701b88a4c39
KirillGrishaev <94450408+KirillGrishaev@users.noreply.github.com>
2021-12-25 10:50:49 +1100

    Delete OtdelCassa.java

--- src/bank/otdel/OtdelCassa.java
+++
@@ -1,8 +0,0 @@
-package bank.otdel;
-
-public class OtdelCassa extends OtdelConstruct {
-    public OtdelCassa(String name, int rabotnik) {
-        this.otdelName = otdelName;
-        this.rabotnik = rabotnik;
-    }
-}
