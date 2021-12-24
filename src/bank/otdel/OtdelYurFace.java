157b4a595489929edc382d76cde5f7a8c1f0f022
KirillGrishaev <94450408+KirillGrishaev@users.noreply.github.com>
2021-12-25 10:51:41 +1100

    Delete OtdelYurFace.java

--- src/bank/otdel/OtdelYurFace.java
+++
@@ -1,7 +0,0 @@
-package bank.otdel;
-
-public class OtdelYurFace extends OtdelConstruct{
-    public OtdelYurFace (String name,int rabotnik){
-        this.otdelName = otdelName;
-        this.rabotnik = rabotnik;}
-}
