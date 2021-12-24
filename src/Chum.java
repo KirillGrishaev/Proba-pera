6a8a0cc57ed9e4b40975ea28494fbf91d8a8d976
KirillGrishaev <94450408+KirillGrishaev@users.noreply.github.com>
2021-12-25 10:38:40 +1100

    Add files via upload

---
+++ src/Chum.java
@@ -0,0 +1,14 @@
+public class Chum {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println("    +");
+        System.out.println("   +++");
+        System.out.println("  +++++");
+        System.out.println(" +++++++");
+        System.out.println("+++++++++");
+        System.out.println("   +++");
+        System.out.println("   +++");
+
+    }
+}
